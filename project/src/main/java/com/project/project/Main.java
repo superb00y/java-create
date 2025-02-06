b6c82e717427f811c05a9e4e7f3c2cb8fc8a00e7
drider nasreddine <krimodrider@gmail.com>
2025-02-06 15:25:28 +0100

    clean version

--- project/src/main/java/com/project/project/Main.java
+++ project/src/main/java/com/project/project/Main.java
@@ -1,1 +1,7 @@
-package com.project.project; public class Main { public static void main(String[] args) { System.out.println("Hello World"); } }
+package com.project.project;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
